cccd18b2697d6b8e86a1413060f0f04b40778165
AB <newuser@Codes-MacBook-Pro-2.local>
2025-12-25 09:54:47 +0400

    feature(apiClient):added ApiCliend,Error handleing, Auto add auth token

---
+++ team-frontend/src/lib/api/axios.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+export const Axios = axios.create({
+    baseURL: import.meta.env.VITE_REACT_PUBLIC_API_URL
+
+})
+
+Axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+
+Axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      const status = error.response.status;
+
+      if (status === 401) {
+        console.error("Unauthorized – token expired");
+      }
+
+      if (status >= 500) {
+        console.error("Server error");
+      }
+    } else {
+      console.error("Network error");
+    }
+
+    return Promise.reject(error);
+  }
+);
