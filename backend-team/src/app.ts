eeddb89af469375470a7f5f1bd9b1af940b0e374
Arthur Tadevosyan <arthur.tadevosyan.dev@gmail.com>
2026-01-09 12:46:55 +0400

    Rebase done

--- backend-team/src/app.ts
+++ backend-team/src/app.ts
@@ -3,12 +3,12 @@ import cors from "cors";
 import helmet from "helmet";
 
 export const app = express();
+app.use(helmet());
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
-app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
