0f9be042a304167a8d1207d6ccfe0213525e2316
harutyunyanA <harutyunyan.artyom75@icloud.com>
2026-01-12 15:09:03 +0400

    feat(api-response):added api response format

---
+++ backend-team/src/utils/response.ts
@@ -0,0 +1,34 @@
+import type { ApiError, ApiResponse } from "../types/api-responses/api";
+import type { Response } from "express";
+
+export function successResponse<T>(
+  res: Response,
+  data: T,
+  statusCode = 200
+): Response<ApiResponse<T>> {
+  const response: ApiResponse<T> = {
+    success: true,
+    data,
+  };
+
+  return res.status(statusCode).send(response);
+}
+
+export function errorResponse(
+  res: Response,
+  code: string,
+  message: string,
+  statusCode = 400
+): Response<ApiResponse<null>> {
+  const error: ApiError = {
+    code,
+    message,
+  };
+
+  const response: ApiResponse<null> = {
+    success: false,
+    error,
+  };
+
+  return res.status(statusCode).send(response);
+}
