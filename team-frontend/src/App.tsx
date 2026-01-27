b61bbcb91a60a9236d2dd74d4ab0e1df2c0ec021
AB <newuser@Codes-MacBook-Pro-2.local>
2026-01-27 13:30:02 +0400

    fix(some feature):removed console.logs and any code

--- team-frontend/src/App.tsx
+++ team-frontend/src/App.tsx
@@ -1,9 +1,6 @@
 import { Layout } from "./layout";
-import { a } from "./lib/api/axios";
 
 export default function App(){
-  console.log(import.meta.env);
-  console.log(a)
   return <>
     <h1 className="font-inter font-semibold">Lumin</h1>
     <Layout/>
