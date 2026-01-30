6b79cdf26fdc6000e753558a2e613ad100193c45
AB <newuser@Codes-MacBook-Pro-2.local>
2026-01-30 19:21:13 +0400

    feat(ProtectedRoute):added protected route for Dashboard

---
+++ team-frontend/src/components/auth/ProtectedRoute.tsx
@@ -0,0 +1,19 @@
+import { Loading } from "../ui/Loading";
+import { useNavigate } from "react-router";
+import { useAuthStore } from "../../stores/authStore";
+import { useEffect } from "react";
+export const ProtectedRoute = ({children}:{children:React.ReactNode}) => {
+    const {isAuthenticated,isLoading}:any = useAuthStore();
+    const navigate = useNavigate();
+    if(isLoading)return <Loading/>
+    console.log("Hello",isAuthenticated)
+    
+    useEffect(() => {
+        if(!isAuthenticated){
+            navigate("/login");
+        }
+    },[isAuthenticated,navigate])
+    return <>
+        {children}
+    </>
+}
