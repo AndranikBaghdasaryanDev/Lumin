ab6d3d5c586be064f2f7bf1b66131554f51b17b2
AB <newuser@Codes-MacBook-Pro-2.local>
2026-01-30 20:34:35 +0400

    feat(router):added routes pages

---
+++ team-frontend/src/routes/index.tsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Layout } from "../layout";
+import { Home } from "../pages/home";
+import { Login } from "../pages/login";
+import { Register } from "../pages/register";
+import { ProtectedRoute } from "../components/auth/ProtectedRoute";
+import { DashBoard } from "../pages/dashboard";
+
+
+export const router = createBrowserRouter([
+  {
+    element: <Layout />, // parent layout
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
+      {
+        path: "/dashboard",
+        element: (
+          <ProtectedRoute>
+            <DashBoard />
+          </ProtectedRoute>
+        ),
+      },
+    ],
+  },
+]);
