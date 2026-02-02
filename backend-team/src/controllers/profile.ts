046258d1abf346a1381ac168597340e320e1e369
testirovchik <erik.hayrapetyan.260@gmail.com>
2026-02-02 18:37:21 +0400

    fix: get profile endpoint

--- backend-team/src/controllers/profile.ts
+++ backend-team/src/controllers/profile.ts
@@ -1,16 +1,25 @@
 import type { NextFunction, Request, Response } from "express";
 import api from "../lib/api.ts";
 import type { ApiResponse } from "../types/api-responses/api.ts";
-import type { userProfile } from "../types/api-responses/profile.ts";
-import { successResponse } from "../utils/response.ts";
+import type { UserProfile } from "../types/api-responses/profile.ts";
+import { errorResponse, successResponse } from "../utils/response.ts";
 
 class ProfileController {
   async get(req: Request, res: Response, next: NextFunction) {
     try {
-      const response = await api.get<ApiResponse<userProfile>>("/profile");
+      const response = await api.get<ApiResponse<UserProfile>>("/profile", {
+        headers: {
+          Authorization: `Bearer ${req.token}`
+        }
+      });
       if (response.data.success) {
         return successResponse(res, response.data.data);
       }
+      return errorResponse(
+        res,
+        response.data.error?.code??"GET_PROFILE_FAILED",
+        response.data.error?.message??"Failed to get profile",
+      )
     } catch (err) {
       next(err);
     }
--- backend-team/src/types/api-responses/profile.ts
+++ backend-team/src/types/api-responses/profile.ts
@@ -1,16 +1,18 @@
-export interface userProfile {
-    id: string,
-    email: string,
-    firstName: string,
-    lastName: string,
-    profileImage: string,
-    phone: string,
-    dateOfBirth: string,
-    studentProfile: {
-      jobTitle: string
-      learningGoal: string,
-      learningInterests: string[],
-      currentStreak: number,
-      totalPoints: number
-    }
+export interface UserProfile {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  profileImage: string;
+  phone: string;
+  dateOfBirth: string;
+  studentProfile: StudentProfile | null;
+}
+
+export interface StudentProfile {
+  jobTitle: string;
+  learningGoal: string;
+  learningInterests: string[];
+  currentStreak: number;
+  totalPoints: number;
 }
