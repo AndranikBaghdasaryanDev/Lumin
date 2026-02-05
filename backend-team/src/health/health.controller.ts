7744fd7fc378a14719789eab1497ee48d551ace7
harutyunyanA <harutyunyan.artyom75@icloud.com>
2026-02-05 16:34:52 +0400

    fix(api key header name): changed api key header name, health endpoint logic

--- backend-team/src/health/health.controller.ts
+++ backend-team/src/health/health.controller.ts
@@ -1,13 +1,31 @@
-import type { Request, Response } from "express"
-import { checkCoreBackend } from "./health.service.ts"
-import { successResponse } from "../utils/response.ts"
+import type { Request, Response } from "express";
+import { checkCoreBackend } from "./health.service.ts";
+import { successResponse } from "../utils/response.ts";
+import logger from "../lib/logger.ts";
 
 export async function healthCheck(req: Request, res: Response) {
-    const isConnected = await checkCoreBackend()
-
-    return successResponse(res, {
-        status: "ok",
-        timestamp: new Date().toISOString(),
-        coreBackend: isConnected ? "connected" : "disconnected"
-    })
+  let startTime = Date.now();
+  logger.info(
+    {
+      method: req.method,
+      url: req.url,
+      headers: req.headers,
+    },
+    "Outgoing Core Backend request",
+  );
+  const isConnected = await checkCoreBackend();
+  logger.info(
+    {
+      method: req.method,
+      url: req.url,
+      status: 200,
+      responseTime: Date.now() - startTime,
+    },
+    "Core Backend response",
+  );
+  return successResponse(res, {
+    status: "ok",
+    timestamp: new Date().toISOString(),
+    coreBackend: isConnected ? "connected" : "disconnected",
+  });
 }
