30c528ffe7d1246758e716583933fd99588ecf50
Narek Khachatryan <narekkhachatryan187@gmail.com>
2026-02-06 15:20:08 +0400

    fix(auth middleware error handling): fixed error handling by using errorResponse function instead of throw

--- backend-team/src/middlewares/auth.middleware.ts
+++ backend-team/src/middlewares/auth.middleware.ts
@@ -1,47 +1,51 @@
 import type { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import env from "../config/env.ts";
+import { errorResponse } from "../utils/response.ts";
 
 export function auth(req: Request, res: Response, next: NextFunction) {
   const authHeader = req.get("authorization");
 
   if (!authHeader) {
-    throw {
-      code: "INVALID_OR_MISSING_TOKEN",
-      message: "Unauthorized",
-      statusCode: 401,
-    };
+    return errorResponse(
+      res,
+      "INVALID_OR_MISSING_TOKEN",
+      "Token is missing",
+      401,
+    );
   }
 
   const [type, token] = authHeader.split(" ");
-  
+
   if (type !== "Bearer" || !token) {
-    throw {
-      code: "INVALID_OR_MISSING_TOKEN",
-      message: "Unauthorized",
-      statusCode: 401,
-    };
+    return errorResponse(
+      res,
+      "INVALID_OR_MISSING_TOKEN",
+      "Token is missing",
+      401,
+    );
   }
 
   try {
     const payload = jwt.verify(token, env.JWT_SECRET);
 
     if (typeof payload === "string") {
-      throw {
-        code: "INVALID_OR_MISSING_TOKEN",
-        message: "Unauthorized",
-        statusCode: 401,
-      };
+      return errorResponse(
+        res,
+        "INVALID_OR_MISSING_TOKEN",
+        "Token is missing",
+        401,
+      );
     }
 
     req.user = payload;
     next();
-
   } catch {
-    throw {
-      code: "INVALID_OR_MISSING_TOKEN",
-      message: "Unauthorized",
-      statusCode: 401,
-    };
+    return errorResponse(
+      res,
+      "INVALID_TOKEN",
+      "Invlalid token",
+      401
+    )
   }
 }
